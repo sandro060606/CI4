b920ece032207a914613326a3a3741d47b9dcb73
Sandro Rodriguez Anchante <sandro661528@gmail.com>
2026-03-27 12:38:19 -0500

    Modelo para Marcas

---
+++ app/Models/MarcaModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MarcaModel extends Model{
+     protected $table = "vehiculos";
+    protected $primaryKey = "id";
+    protected $returnType = "array";
+    protected $allowedFields = ["marca"];
+    //Campos de Auditoria => ¿Cuando se Creó?, ¿Cuando se Modifico?
+    protected $useTimestamps = true;
+    protected $createdField = "created_at"; //Campo Tabla Vehiculos
+    protected $updatedField = "updated_at"; //Campo Tabla Vehiculos
+}
